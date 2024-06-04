96df620c41e504b838242333179a26be89e1e9d3
Omarhudaib <168991559+Omarhudaib@users.noreply.github.com>
2024-06-04 22:16:50 +0300

    Add files via upload

---
+++ array_basic.js
@@ -0,0 +1,87 @@
+// 1-Write a function to find the largest element in an array.
+/*
+const cars = [55,44,66];
+function largest(array){
+    let o =array[0];
+ for(let i=0; i<array.length ;i++){
+    if (array[i]>o){o=array[i];}
+ }
+console.log(o)
+
+}
+// 2-Write a function to find the smallest element in an array.
+function smolest(array){
+let s =array[0];
+for(let i =1;i<array.length;i++)
+    if(s>array[i]){
+        s=array[i];
+    }
+    console.log(s)
+}
+// 3-Write a function to find the sum of all elements in an array.
+const omar = [11,5,4,5,6,7,];
+function sum(array){
+    let s =array[0];
+    for(let i =1;i<array.length;i++)
+    s= s+array[i]
+        console.log(s)
+    }
+     sum(omar)
+// 4-Write a function to find the average of all elements in an array.
+const omar = [11,5,4,5,6,7,];
+function sum(array){
+    let s =array[0];
+    for(let i =1;i<array.length;i++)
+    s= s+array[i]
+        console.log(s /array.length)
+    }
+     sum(omar)
+// 5-Write a function to find the median of all elements in an array.
+const omar = [11,5,4,5,6,7,];
+function m(array){
+    array.sort((a,b) =>a-b);
+    const n =array.length;
+    if(n % 2==1){
+        console.log(array[Math.floor(n/2)])
+    }
+    else
+   {
+        console.log( ( (array[(n/2)-1])+(array[n/2]) )/2 );
+    }
+
+}
+// 6-Write a function to remove all duplicates from an array.
+const omar = [11,5,5,5,6,7,];
+function del(array){
+    let s =array[0];
+    for(let i =1;i<array.length;i++)
+    s=array[i];
+    array[i].popped
+        console.log(array)
+    }
+     sum(omar)
+// 7-Write a function to sort an array in ascending order.
+const omar = [11,5,5,5,6,7,];
+function re(array){
+const o= new Set(array);
+array=o
+console.log(array); 
+}
+re(omar)
+
+// 8-Write a function to sort an array in descending order.
+const omar = [1,2,3,6,4];
+function s(array){
+    array.sort((a,b) =>a-b);
+    const n =reverse(array);
+    console.log(n)
+
+
+}
+// 9-Write a function to shuffle the elements of an array randomly.
+function t(array){
+    for(let i =array.length -1; i>0;i--)
+const o= Math.floor(Math.random(()*(i +1)));
+console.log(o)
+}
+t(omar)*/
